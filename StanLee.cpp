880786c2c8227bde1d86c4758082c44417288e32
Pedro <pedro.carrascoc@mail.udp.cl>
2018-12-05 23:32:20 -0300

    Base Classes

---
+++ StanLee.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+class Memory
+{
+public:
+	Memory();
+	~Memory();
+	
+};
+
+class StanLeeFS
+{
+public:
+	StanLeeFS();
+	~StanLeeFS();
+	
+};
+
+class Shell
+{
+public:
+	Shell();
+	~Shell();
+	
+};
