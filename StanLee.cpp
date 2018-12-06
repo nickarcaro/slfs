c07ee52613661715a35c498aedd03803d8150c2f
nicolasaraya77 <nicolas.arayac@mail.udp.cl>
2018-12-06 14:48:40 -0300

    Update StanLee.cpp
    a

--- StanLee.cpp
+++ StanLee.cpp
@@ -2,39 +2,20 @@
 #include <vector>
 using namespace std;
 
-class Memory
-{
-public:
-	Memory();
-	~Memory();
+const static int counter = 4096;
+
+struct Inodo {
+	int ninodo;
+	bool tipo;
+	int fechacreacion;
+	int ftamano;
+	int blocks[counter];
 	
+
 };
 
-class StanLeeFS
-{
-public:
-	StanLeeFS();
-	~StanLeeFS();
-	
-void createDir(){
+int main(){
+	cout<<"gola";
+	return 0;
 }
 
-void createFile(){
-}
-	
-void updateFile(){
-}
-
-void deleteFile(){
-}
-	
-};
-
-class Shell
-{
-public:
-	Shell();
-	~Shell();
-	
-	
-};
